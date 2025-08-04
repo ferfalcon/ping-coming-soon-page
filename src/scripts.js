47a0db02dce54ba79a12114725e43d43ef2f4386
Fer <fer.falcon.dg@gmail.com>
2025-08-04 10:49:18 -0300

    Desktop font sizes

--- src/scripts.js
+++ src/scripts.js
@@ -1,24 +1,54 @@
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const emailInput = document.getElementById('email');
+const form = document.querySelector('.main-form');
+const message = document.getElementById('validationMessage');
+const mainHeading = document.querySelector('.main-heading');
+
 emailInput.addEventListener("blur", checkEmail);
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const emailValue = emailInput.value;
+
+    if (!validateEmail(emailValue)) {
+        message.textContent = "Please provide a valid email address.";
+        message.classList.add('error');
+        emailInput.classList.add('error');
+    } else {
+        message.textContent = "";
+        message.classList.remove('error');
+        emailInput.classList.remove('error');
+
+        form.classList.add('submited')
+
+        const confirmation = document.createElement('p');
+        confirmation.textContent = "Thanks! For your subscription.";
+        confirmation.classList.add('confirmation-message');
+
+        mainHeading.insertAdjacentElement('afterend', confirmation);
+    }
+});
 
 function validateEmail(email) {
     return emailRegex.test(email);
 }
 
 function checkEmail() {
-    const emailValue = document.getElementById('email').value;
-    const message = document.getElementById('validationMessage');
+    const emailValue = emailInput.value;
 
-    if (emailValue != "") {
+    if (emailValue !== "") {
         if (!validateEmail(emailValue)) {
             message.textContent = "Please provide a valid email address.";
             message.classList.add('error');
             emailInput.classList.add('error');
         } else {
             message.textContent = "";
+            message.classList.remove('error');
+            emailInput.classList.remove('error');
         }
     } else {
+        message.textContent = "";
         message.classList.remove('error');
         emailInput.classList.remove('error');
     }
